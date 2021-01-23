e76fc38430aeae8cc3c9c79e7ace875402d4f54c
estasney <estasney@users.noreply.github.com>
2021-01-23 00:21:38 -0500

    Remove Pyro Add npsock.py

--- yolov5/remote/__init__.py
+++ yolov5/remote/__init__.py
@@ -1,13 +1,12 @@
 from typing import Union, Dict, List
 
-import Pyro5.api
 import numpy as np
 
 from yolov5 import PretrainedWeights
 from yolov5.models.slim import SlimModelRunner
+from yolov5.remote.npsock import NpSocketBase
 
 
-@Pyro5.api.expose
 class RemoteModelDetector(SlimModelRunner):
     def __init__(self, params: Union[Dict, str] = 'scratch',
                  weights: str = PretrainedWeights.SMALL,
@@ -15,15 +14,5 @@ class RemoteModelDetector(SlimModelRunner):
         super().__init__(params, weights, device, img_size)
 
     def detect(self, source: List[np.array], img_size=640, conf=None, iou=None):
-        # return super().detect(source, img_size, conf, iou)
-        print("Got Called")
-
-
-if __name__ == '__main__':
-    daemon = Pyro5.api.Daemon()
-    ns = Pyro5.api.locate_ns()
-    uri = daemon.register(RemoteModelDetector)
-    ns.register("detect", uri)
-    print("Registed {}".format(uri))
-    daemon.requestLoop()
+        return super().detect(source, img_size, conf, iou)
 
